0e07dfec0b050db9a9039789a06b3ce180b3876d
BananaPilot <fabiopetrosillo2@gmail.com>
2024-04-04 17:26:38 +0200

    first commit

---
+++ src/main/java/com/teamproject1/scuoledevelhope/config/ScuoleDevelhopeApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.teamproject1.scuoledevelhope.config;
+
+import com.bananapilot.samplespringauthenticationframework.config.SampleSpringAuthenticationFrameworkConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(SampleSpringAuthenticationFrameworkConfig.class)
+public class ScuoleDevelhopeApplicationConfig {
+
+}
---
+++ src/test/java/com/teamproject1/scuoledevelhope/ScuoleDevelhopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject1.scuoledevelhope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuoleDevelhopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
