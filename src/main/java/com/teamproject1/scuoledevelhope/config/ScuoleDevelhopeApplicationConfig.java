0f8e5bcf4a60cf8503b98dc31686bca5317517fb
BananaPilot <fabiopetrosillo2@gmail.com>
2024-04-05 14:59:14 +0200

    updated pom.xml + gitignore

--- src/main/java/com/teamproject1/scuoledevelhope/config/ScuoleDevelhopeApplicationConfig.java
+++ src/main/java/com/teamproject1/scuoledevelhope/config/ScuoleDevelhopeApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.teamproject1.scuoledevelhope.config;
 
+
 import com.bananapilot.samplespringauthenticationframework.config.SampleSpringAuthenticationFrameworkConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
