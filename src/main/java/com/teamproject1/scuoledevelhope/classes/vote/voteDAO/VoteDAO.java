6504ee4aae866fc7e59d87d68524195b31201bdc
Simona <labib.simona@gmail.com>
2024-04-17 11:04:28 +0200

    feature/3tier-Vote

---
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/vote/voteDAO/VoteDAO.java
@@ -0,0 +1,10 @@
+package com.teamproject1.scuoledevelhope.classes.vote.voteDAO;
+import com.teamproject1.scuoledevelhope.classes.vote.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VoteDAO extends JpaRepository<Vote, UUID> {
+}
