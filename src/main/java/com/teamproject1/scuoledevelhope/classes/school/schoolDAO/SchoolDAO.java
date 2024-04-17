c5e7db01e495f44b12e1a1d416b2960dbdbf1ace
Simona <labib.simona@gmail.com>
2024-04-17 11:16:29 +0200

    feature/3tier-School

---
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/school/schoolDAO/SchoolDAO.java
@@ -0,0 +1,9 @@
+package com.teamproject1.scuoledevelhope.classes.school.schoolDAO;
+
+import com.teamproject1.scuoledevelhope.classes.school.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SchoolDAO extends JpaRepository<School, UUID> {
+}
---
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/school/schoolService/SchoolService.java
@@ -0,0 +1,48 @@
+package com.teamproject1.scuoledevelhope.classes.school.schoolService;
+
+import com.teamproject1.scuoledevelhope.classes.school.School;
+import com.teamproject1.scuoledevelhope.classes.school.schoolDAO.SchoolDAO;
+import com.teamproject1.scuoledevelhope.types.BaseResponseElement;
+import com.teamproject1.scuoledevelhope.types.BaseResponseList;
+import com.teamproject1.scuoledevelhope.types.errors.SQLException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class SchoolService {
+
+    SchoolDAO schoolDAO;
+
+    public SchoolService(SchoolDAO schoolDAO) {
+        this.schoolDAO = schoolDAO;
+    }
+
+    public BaseResponseList<School> findAll(){
+        return new BaseResponseList<>(schoolDAO.findAll());
+    }
+
+    public BaseResponseElement<School> findById(UUID id){
+        Optional<School> result = schoolDAO.findById(id);
+        if(result.isEmpty()){
+            throw new SQLException("Register was not present");
+        }
+        return new BaseResponseElement<>(result.get());
+    }
+
+    public BaseResponseElement<School> save(School school) {
+        return new BaseResponseElement<>(schoolDAO.save(school));
+    }
+
+    public BaseResponseElement<School> deleteById(UUID id){
+        Optional<School> temp = schoolDAO.findById(id);
+
+        if(temp.isEmpty()){
+            throw new SQLException("Register was not present");
+        }
+        schoolDAO.deleteById(id);
+
+        return new BaseResponseElement<>(temp.get());
+    }
+}
