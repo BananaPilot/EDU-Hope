000b3192d0b3744f24605a0a438d3da84413e098
BananaPilot <fabiopetrosillo2@gmail.com>
2024-04-23 02:24:42 +0200

    try

--- src/main/java/com/teamproject1/scuoledevelhope/classes/meeting/dao/MeetingDAO.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/meeting/dao/MeetingDAO.java
@@ -9,44 +9,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface MeetingDAO extends JpaRepository<Meeting, Long> {
-    //----------- STUDENT----------//
-    //TUTTI I MEETING DI UN SINGOLO STUDENTE
-    @Query(value = "select meeting.* from meeting\n" +
-            "join meeting_student on meeting.id_meeting = meeting_student.id_meeting_fk \n" +
-            "where meeting_student.id_student_fk = :idStudent", nativeQuery = true)
-    List<Meeting> allByStudentId(@Param("idStudent") Long idStudent);
+    @Query(nativeQuery = true, value = "")
+    List<Meeting> getAllById(@Param("id") Long id);
 
-    //MEETING DI STUDENTE IN UN INTERVALLO DI TEMPO
-    @Query(value = "select meeting.* from meeting\n" +
-            "join meeting_student on meeting.id_meeting = meeting_student.id_meeting_fk \n" +
-            "where meeting_student.id_student_fk = :idStudent\n" +
-            "and meeting.start_date > :startDate and meeting.start_date < :endDate", nativeQuery = true)
-    List<Meeting> intervalStudentId(@Param("idStudent") Long idStudent, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
-
-
-    //----------- TUTOR ----------//
-
-    //TUTTI I MEETING DI UN SINGOLO TUTOR
-    @Query(value = "select * from meeting m where tutor_id_fk = :idTutor", nativeQuery = true)
-    List<Meeting> allByTutorId(@Param("idTutor") Long idTutor);
-
-    //TUTTI I MEETING DI UN SINGOLO TUTOR IN UN INTERVALLO DI TEMPO
-    @Query(value = "select * from meeting \n" +
-            "where tutor_id_fk = :idTutor\n" +
-            "and meeting.start_date > :startDate and meeting.start_date < :endDate", nativeQuery = true)
-    List<Meeting> intervalTutorId(@Param("idTutor") Long idTutor, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
-
-    //----------- END TUTOR ----------//
-    //----------- COORDINATOR ----------//
-    //TUTTI I MEETING DI UN SINGOLO COORDINATORE
-    @Query(value = "select * from meeting m where coordinator_id_fk = :idCoordinator", nativeQuery = true)
-    List<Meeting> allByCoordinatorId(@Param("idCoordinator") Long idCoordinator);
-
-    //TUTTI I MEETING DI UN SINGOLO COORDINATORE IN UN INTERVALLO DI TEMPO
-    @Query(value = "select * from meeting \n" +
-            "where coordinator_id_fk = :idCoordinator\n" +
-            "and meeting.start_date > :startDate and meeting.start_date < :endDate", nativeQuery = true)
-    List<Meeting> intervalCoordinatorId(@Param("idCoordinator") Long idCoordinator, LocalDate startDate, LocalDate endDate);
-
-    //----------- END COORDINATOR ----------//
+    @Query(nativeQuery = true, value = " where ms.id_student = :id and meeting.start_date > :startDate and meeting.start_date < :endDate")
+    List<Meeting> getMeetingsInInterval(@Param("id") Long id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
--- src/main/java/com/teamproject1/scuoledevelhope/classes/meeting/service/MeetingService.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/meeting/service/MeetingService.java
@@ -16,47 +16,18 @@ import java.util.List;
 public class MeetingService {
 
     MeetingDAO meetingDAO;
-    UserRegistryDAO urDAO;
 
     public MeetingService(MeetingDAO meetingDAO) {
         this.meetingDAO = meetingDAO;
     }
 
-    //----------- STUDENTE----------//
-    public BaseResponseList<Meeting> allByStudentId(Long id) {
-        return new BaseResponseList<>(meetingDAO.allByStudentId(id));
+    public BaseResponseList<Meeting> getAllById(Long id) {
+        return new BaseResponseList<>(meetingDAO.getAllById(id));
     }
 
-    public BaseResponseList<Meeting> intervalStudentId(Long idStudent, LocalDate startDate, LocalDate endDate) {
-        return new BaseResponseList<>(meetingDAO.intervalStudentId(idStudent, startDate, endDate));
+    public BaseResponseList<Meeting> getByIntervalWithId(Long id, LocalDate startDate, LocalDate endDate) {
+        return new BaseResponseList<>(meetingDAO.getMeetingsInInterval(id, startDate, endDate));
     }
-
-    public List<UserRegistry> allStudentsByMeeting(Long iDmeeting) {
-        return urDAO.allStudentsByMeeting(iDmeeting);
-    }
-    //----------- END STUDENTE----------//
-
-    //----------- TUTOR ----------//
-    public BaseResponseList<Meeting> allByTutorId(Long id) {
-        return new BaseResponseList<>(meetingDAO.allByTutorId(id));
-    }
-
-    public BaseResponseList<Meeting> intervalTutorId(Long idTutor, LocalDate startDate, LocalDate endDate) {
-        return new BaseResponseList<>(meetingDAO.intervalTutorId(idTutor, startDate, endDate));
-    }
-
-    //----------- END TUTOR ----------//
-
-    //----------- COORDINATOR ----------//
-    public BaseResponseList<Meeting> allByCoordinatorId(Long id) {
-        return new BaseResponseList<>(meetingDAO.allByCoordinatorId(id));
-    }
-
-    public BaseResponseList<Meeting> intervalCoordinatorId(Long idCoordinator, LocalDate startDate, LocalDate endDate) {
-        return new BaseResponseList<>(meetingDAO.intervalCoordinatorId(idCoordinator, startDate, endDate));
-    }
-
-    //----------- END COORDINATOR ----------//
 
     public BaseResponseElement<Meeting> save(Meeting meeting) {
         LocalDateTime start_date = meeting.getStartDate();
@@ -66,5 +37,4 @@ public class MeetingService {
         }
         return new BaseResponseElement<Meeting>(meetingDAO.save(meeting));
     }
-
 }
