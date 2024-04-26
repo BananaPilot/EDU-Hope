081250939d85c84d67a36a9ec4da9686c3aee2e0
BananaPilot <fabiopetrosillo2@gmail.com>
2024-04-26 10:27:04 +0200

    removed autowireing of userMiddleware since it does not exist anymore

--- src/main/java/com/teamproject1/scuoledevelhope/Middlewares.java
+++ src/main/java/com/teamproject1/scuoledevelhope/Middlewares.java
@@ -1,7 +1,6 @@
 package com.teamproject1.scuoledevelhope;
 
 import com.teamproject1.scuoledevelhope.classes.calendar.CalendarInterceptor;
-import com.teamproject1.scuoledevelhope.classes.user.UserMiddleware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,14 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Middlewares implements WebMvcConfigurer {
 
     @Autowired
-    UserMiddleware userMiddleware;
-
-    @Autowired
     CalendarInterceptor calendarInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(userMiddleware);
         registry.addInterceptor(calendarInterceptor);
     }
 }
