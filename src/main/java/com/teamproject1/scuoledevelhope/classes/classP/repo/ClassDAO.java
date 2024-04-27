0f4a3a18ed3992d2db5928fc69d677477c87e739
BananaPilot <fabiopetrosillo2@gmail.com>
2024-04-27 19:25:11 +0200

    added delete

--- src/main/java/com/teamproject1/scuoledevelhope/classes/classP/repo/ClassDAO.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/classP/repo/ClassDAO.java
@@ -1,9 +1,18 @@
 package com.teamproject1.scuoledevelhope.classes.classP.repo;
 
 import com.teamproject1.scuoledevelhope.classes.classP.Classes;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClassDAO extends JpaRepository<Classes, Long> {
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from class where id_class = :id", nativeQuery = true)
+    int deleteClassById(@Param("id") Long id);
 }
