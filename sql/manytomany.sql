d5ce65f940ce686b4459416401e2d3ac897f17a1
BananaPilot <fabiopetrosillo2@gmail.com>
2024-05-02 18:57:21 +0200

    chambiato sql e reformattato

--- sql/manytomany.sql
+++ sql/manytomany.sql
@@ -6,12 +6,3 @@ create table if not exists user_role
     foreign key (id_user) references user (id) on delete cascade,
     primary key (id_user, id_role)
 );
-
-create table if not exists user_meeting
-(
-    id_user bigint,
-    id_meeting bigint,
-    foreign key (id_user) references user(id),
-    foreign key (id_meeting) references meeting (id_meeting),
-    primary key (id_meeting, id_user)
-);
