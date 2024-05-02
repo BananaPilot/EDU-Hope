c551499f2dfbaf4d08633bb850502fe83368bad8
BananaPilot <fabiopetrosillo2@gmail.com>
2024-05-02 18:38:33 +0200

    added all args constructor

--- src/main/java/com/teamproject1/scuoledevelhope/classes/calendar/Calendar.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/calendar/Calendar.java
@@ -2,6 +2,7 @@ package com.teamproject1.scuoledevelhope.classes.calendar;
 
 import com.teamproject1.scuoledevelhope.classes.calendar.meeting.MeetingResponse;
 import com.teamproject1.scuoledevelhope.types.dtos.Pagination;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -13,9 +14,14 @@ public class Calendar extends Pagination {
     private LocalDate endDate;
     private List<MeetingResponse> calendar = new ArrayList<>();
 
-    public Calendar() {
-    }
+    public Calendar() {}
 
+    public Calendar(HttpStatus httpStatus, String message, String description, int page, int pageSize, long totalElements, int totalPages, LocalDate startDate, LocalDate endDate, List<MeetingResponse> calendar) {
+        super(httpStatus, message, description, page, pageSize, totalElements, totalPages);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.calendar = calendar;
+    }
 
     public LocalDate getStartDate() {
         return startDate;
