94a813562dfa7d8a04bf10c66aeb9d6407e47ac7
BananaPilot <fabiopetrosillo2@gmail.com>
2024-05-04 12:19:52 +0200

    added test dir and application.yaml
    to run the profile follow the same pattern presented in the
    ScuoleDevelhopeApplicationTests.class

--- src/test/java/com/teamproject1/scuoledevelhope/ScuoleDevelhopeApplicationTests.java
+++ src/test/java/com/teamproject1/scuoledevelhope/ScuoleDevelhopeApplicationTests.java
@@ -2,12 +2,15 @@ package com.teamproject1.scuoledevelhope;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScuoleDevelhopeApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
