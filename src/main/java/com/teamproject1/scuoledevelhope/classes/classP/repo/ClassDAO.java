4a17cb3c64fe4b99f223398648de514a71b91765
BananaPilot <fabiopetrosillo2@gmail.com>
2024-05-07 01:47:03 +0200

    dao fixed

--- src/main/java/com/teamproject1/scuoledevelhope/classes/classP/repo/ClassDAO.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/classP/repo/ClassDAO.java
@@ -2,6 +2,8 @@ package com.teamproject1.scuoledevelhope.classes.classP.repo;
 
 import com.teamproject1.scuoledevelhope.classes.classP.Classes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
