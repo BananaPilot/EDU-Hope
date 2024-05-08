cb6170fc9baf8b144ad8765cce557d19085a819d
BananaPilot <fabiopetrosillo2@gmail.com>
2024-05-08 13:09:56 +0200

    commit

--- src/main/java/com/teamproject1/scuoledevelhope/classes/register/repo/RegisterDao.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/register/repo/RegisterDao.java
@@ -1,6 +1,7 @@
 package com.teamproject1.scuoledevelhope.classes.register.repo;
 
 import com.teamproject1.scuoledevelhope.classes.register.Register;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
