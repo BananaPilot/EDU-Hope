81cee166bed45cc46166d8baf2cf0a78dc432930
Simona Labib <152871220+SimonaLabib@users.noreply.github.com>
2024-05-26 18:29:29 +0200

    Update CoordinatorDAO.java

--- src/main/java/com/teamproject1/scuoledevelhope/classes/coordinator/repo/CoordinatorDAO.java
+++ src/main/java/com/teamproject1/scuoledevelhope/classes/coordinator/repo/CoordinatorDAO.java
@@ -1,10 +1,7 @@
 package com.teamproject1.scuoledevelhope.classes.coordinator.repo;
 
 import com.teamproject1.scuoledevelhope.classes.coordinator.Coordinator;
-import com.teamproject1.scuoledevelhope.classes.tutor.Tutor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
